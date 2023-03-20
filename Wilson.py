94bce408d3ec2e5447a5b9be32cff2c881d99d42
Kalai6vani <90702110+Kalai6vani@users.noreply.github.com>
2023-03-20 20:57:57 +0530

    Create Wilson.py

---
+++ Wilson.py
@@ -0,0 +1,14 @@
+''' WILSON:
+
+Wilson is a typewriter. He is working in the government office. He needs to remove the blank spaces
+in the string. Help him to remove the blank spaces in the string.
+
+SAMPLE INPUT:
+raja is a good boy
+SAMPLE OUTPUT:
+rajaisagoodboy
+
+SOLUTION:'''
+
+S=input()
+print(S.replace(" ",''))
